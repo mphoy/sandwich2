55a554f243eb285fd076b23c618870ae218efd35
mphoy <mmbadaligampho@gmail.com>
2026-05-12 16:09:33 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/jsandwich/JSandwich.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jsandwich;
+
+/**
+ *
+ * @author 23005006
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class JSandwich {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("sandwich spaza shop");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(732, 716);
+        
+        
+        
+        System.out.println("Hello World!");
+    }
+}
